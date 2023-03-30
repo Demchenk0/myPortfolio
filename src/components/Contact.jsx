f791b74a1ffef6b581cc4552dbae2420ac1255d7
Demchenk0 <santadyr2016@gmail.com>
2023-03-30 19:16:17 +0300

    skroll

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
       <form method='POST' action="https://getform.io/f/b9f9619b-f219-4330-8531-1d8ad1344d29" className='flex flex-col max-w-[600px] w-full'>
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
-          <p className='text-gray-300 py-4'>*Submit the form below or shoot me an email - myemail@gmail.com </p>
+          <p className='text-gray-300 py-4'> * You can contact me by filling out this form </p>
         </div>
         <input className=' bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
         <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
