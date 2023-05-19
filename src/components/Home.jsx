a92dbfba0cde131d11712a90e3e3f00c659524bd
Demchenk0 <santadyr2016@gmail.com>
2023-05-19 15:27:53 +0300

    text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,9 +15,8 @@ const Home = () => {
 					I'm a Full Stack Developer.
 				</h2>
 				<p className="text-[#8892b0] py-4 max-w-[700px]">
-					I'm a full-stack developer specializing in building (and occasionally
-					designing) exceptional digital experiences. Currently, I'm focused on
-					building responsive full-stack web applications.
+				I am a Junior Full Stack developer, specializing at HTML5,
+CSS3, React, Node.js, SASS, Git, JavaScript, REST API, Redux, MongoDB. Looking for a permanent job in a fast-growing company with a friendly team, where I can improve my skills, learn new way of solvin tasks, develop HARD/SOFT skills. As a specialist I strive for the result, but I don't forget about the process and teamwork.
 				</p>
 				<div>
 					<button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600 duration-300">
